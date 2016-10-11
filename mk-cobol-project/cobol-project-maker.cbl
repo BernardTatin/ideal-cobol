026810a5a24ec9e31a6d7488fb69dc3f9cf17930
bernard <bernard@debian>
2016-10-11 23:59:23 +0200

    Slow start, but I can do what I want

--- mk-cobol-project/cobol-project-maker.cbl
+++ mk-cobol-project/cobol-project-maker.cbl
@@ -16,10 +16,24 @@
        PROCEDURE DIVISION.
       *>   reading the first argument of the comand line
       *>   put it in project-name.
-      *>   Open Cobol specific I think.
+      *>   Open Cobol specific I think, set -std of cobc at standard.
       *>   TODO find a way to control string size here 
            ACCEPT project-name FROM COMMAND-LINE
            END-ACCEPT.
+           IF project-name = SPACES
+      *>       if no arguments, show an error
+      *>       under Unix, it's like writing on stderr     
+               DISPLAY "cobol-project-maker needs a project file name" 
+                 UPON SYSERR
+               END-DISPLAY
+               perform 900-TERMINATE
+           END-IF.
+           DISPLAY "We will create the project " project-name
+           END-DISPLAY.
+           perform 900-TERMINATE.
+           
+       900-TERMINATE SECTION.
+           STOP RUN.
 
       *> end of the game
        STOP RUN.
