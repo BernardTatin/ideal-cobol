6f46bfb1b6d1df05c6e864770d34f4dc461871a5
BernardTatin <bernard.tatin@outlook.fr>
2016-10-19 22:43:58 +0200

    Dyslexia bug fixed

--- mk-cobol-project/create-cobolfile.cbl
+++ mk-cobol-project/create-cobolfile.cbl
@@ -2,27 +2,19 @@
          PROGRAM-ID. create-cobolfile.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-           01 line-app PIC X(256) VALUE "APP = :".
-           01 template.
-               05 line-01 PIC X(256) VALUE "APP = cobol-project-maker".
-               05 line-02 PIC X(256) VALUE "MAIN = $(APP).cbl".
-               05 line-03 PIC X(256) VALUE "CPYBOOKS = ".
-               05 line-04 PIC X(256) VALUE "TESTS_ARGS = coco-project".
-               05 line-05 PIC X(256) VALUE "_dll = create-makefile" &
-                   " create-cobolfile".
-               05 line-06 PIC X(256) VALUE "".
-               05 line-07 PIC X(256) VALUE "include ../mk/common.mk".
            01 buffer PIC X(256).
          LINKAGE SECTION.
       *> project name, the first argument on the command line   
            01 project-name PIC X(256).
        PROCEDURE DIVISION USING project-name.
        prog.
-           STRING line-app DELIMITED BY ":" 
-                   project-name DELIMITED BY SPACES
-                   INTO buffer
-           END STRING.
-           DISPLAY buffer END-DISPLAY.
+           STRING "Writing source file for " DELIMITED BY SIZE
+               project-name DELIMITED BY SPACES
+               " project" DELIMITED BY SIZE
+               INTO buffer
+           END-STRING.
+           DISPLAY buffer
+           END-DISPLAY.
            EXIT PROGRAM.
       *> emergency exit
        900-TERMINATE SECTION.
