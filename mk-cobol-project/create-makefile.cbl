733111be3dcbdb4bb44e62ec980685a3b1648529
BernardTatin <bernard.tatin@outlook.fr>
2016-10-19 22:59:16 +0200

    Using an array

--- mk-cobol-project/create-makefile.cbl
+++ mk-cobol-project/create-makefile.cbl
@@ -1,34 +1,40 @@
        IDENTIFICATION DIVISION.
-         PROGRAM-ID. create-makefile.
+       PROGRAM-ID. create-makefile.
        DATA DIVISION.
-         WORKING-STORAGE SECTION.
-           01 line-app PIC X(256) VALUE "APP = :".
-           01 template.
-               05 line-01 PIC X(256) VALUE "APP = cobol-project-maker".
+       WORKING-STORAGE SECTION.
+       01 line-app PIC X(256) VALUE "APP = :".
+       01 template.
+               05 line-01 PIC X(256) VALUE "".
                05 line-02 PIC X(256) VALUE "MAIN = $(APP).cbl".
                05 line-03 PIC X(256) VALUE "CPYBOOKS = ".
                05 line-04 PIC X(256) VALUE "TESTS_ARGS = coco-project".
                05 line-05 PIC X(256) VALUE "_dll = create-makefile" &
-                   " create-cobolfile".
+               " create-cobolfile".
                05 line-06 PIC X(256) VALUE "".
                05 line-07 PIC X(256) VALUE "include ../mk/common.mk".
-           01 buffer PIC X(256).
-         LINKAGE SECTION.
+       01 all-lines REDEFINES template.
+               05 one-line OCCURS 7 PIC X(256).
+       01 i PIC 9.
+       01 buffer PIC X(256).
+       LINKAGE SECTION.
       *> project name, the first argument on the command line   
-           01 project-name PIC X(256).
-       PROCEDURE DIVISION USING project-name.
+       01 project-name PIC X(256).
+           PROCEDURE DIVISION USING project-name.
        prog.
            STRING line-app DELIMITED BY ":" 
-                   project-name DELIMITED BY SPACES
-                   INTO buffer
+           project-name DELIMITED BY SPACES
+           INTO buffer
            END-STRING.
            DISPLAY buffer END-DISPLAY.
+           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 7
+               DISPLAY one-line(i) END-DISPLAY
+           END-PERFORM.
            EXIT PROGRAM.
       *> emergency exit
        900-TERMINATE SECTION.
            DISPLAY "FATAL ERROR when witing Makefile, exit"
-              UPON SYSERR
+           UPON SYSERR
            END-DISPLAY.
            STOP RUN.
 
-       END PROGRAM create-makefile.
+           END PROGRAM create-makefile.
