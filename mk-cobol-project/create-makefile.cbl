f372e2520df65257ce57ddf45d1c3d3e5b7d6f78
Bernard Tatin <bernard.tatin@outlook.fr>
2016-10-21 00:37:52 +0200

    Ready to text.

--- mk-cobol-project/create-makefile.cbl
+++ mk-cobol-project/create-makefile.cbl
@@ -4,14 +4,14 @@
        WORKING-STORAGE SECTION.
        01 line-app PIC X(256) VALUE "APP = :".
        01 template.
-               05 line-01 PIC X(256) VALUE ":".
-               05 line-02 PIC X(256) VALUE "MAIN = $(APP).cbl:".
-               05 line-03 PIC X(256) VALUE "CPYBOOKS = :".
-               05 line-04 PIC X(256) VALUE "TESTS_ARGS = coco-project:".
+               05 line-01 PIC X(256) VALUE "#".
+               05 line-02 PIC X(256) VALUE "MAIN = $(APP).cbl".
+               05 line-03 PIC X(256) VALUE "CPYBOOKS = ".
+               05 line-04 PIC X(256) VALUE "TESTS_ARGS = coco-project".
                05 line-05 PIC X(256) VALUE "_dll = create-makefile" &
-               " create-cobolfile:".
-               05 line-06 PIC X(256) VALUE ":".
-               05 line-07 PIC X(256) VALUE "include ../mk/common.mk:".
+               " create-cobolfile".
+               05 line-06 PIC X(256) VALUE "#".
+               05 line-07 PIC X(256) VALUE "include ../mk/common.mk".
        01 all-lines REDEFINES template.
                05 one-line OCCURS 7 PIC X(256).
        01 i PIC 9.
@@ -29,8 +29,9 @@
            DISPLAY buffer END-DISPLAY.
            PERFORM VARYING i FROM 1 BY 1 UNTIL i > 7
                MOVE "#" TO buffer
-               STRING one-line(i) DELIMITED BY ":" INTO buffer
-               END-STRING
+               MOVE one-line(i) to buffer
+      *         STRING one-line(i) DELIMITED BY ":" INTO buffer
+      *         END-STRING
                MOVE FUNCTION LENGTH ( FUNCTION TRIM (buffer TRAILING) )
                TO blen
                DISPLAY buffer(1:blen) 
