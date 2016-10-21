4261e1bdffdee787a35ac3991aaafaa81ee45346
Bernard Tatin <bernard.tatin@outlook.fr>
2016-10-21 23:18:57 +0200

    My first big bug (trimright fails)

--- mk-cobol-project/create-makefile.cbl
+++ mk-cobol-project/create-makefile.cbl
@@ -1,37 +1,49 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. create-makefile.
+       PROGRAM-ID. create-makefile. IS initial program.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+       01 line-tit PIC X(256) VALUE "# Makefile :".
        01 line-app PIC X(256) VALUE "APP = :".
        01 template.
-               05 line-01 PIC X(256) VALUE "#".
+               05 line-aa PIC X(256) VALUE "# =====================" &
+                  "===========================================".
+               05 line-ab PIC X(256) VALUE "#x".
+               05 line-ac PIC X(256) VALUE "# export " &
+                  "LD_LIBRARY_PATH=$(pwd)/bin".
+               05 line-ad PIC X(256) VALUE "# =====================" &
+                  "===========================================".
+               05 line-ae PIC X(256) VALUE "".
+               05 line-00 PIC X(256) VALUE "#x".
+               05 line-01 PIC X(256) VALUE "".
+               05 line-04 PIC X(256) VALUE "TESTS-ARGS =".
                05 line-02 PIC X(256) VALUE "MAIN = $(APP).cbl".
                05 line-03 PIC X(256) VALUE "CPYBOOKS = ".
-               05 line-04 PIC X(256) VALUE "TESTS_ARGS = coco-project".
                05 line-05 PIC X(256) VALUE "_dll = create-makefile" &
                " create-cobolfile".
                05 line-06 PIC X(256) VALUE "#".
                05 line-07 PIC X(256) VALUE "include ../mk/common.mk".
        01 all-lines REDEFINES template.
-               05 one-line OCCURS 7 PIC X(256).
+               05 one-line OCCURS 50 PIC X(256).
        01 i PIC 9.
-       01 blen PIC 999.
-       01 buffer PIC X(256) USAGE DISPLAY.
-       01 out-buffer PIC X(256).
+       01 buffer PIC X(1256).
        LINKAGE SECTION.
       *> project name, the first argument on the command line
        01 project-name PIC X(256).
 
        PROCEDURE DIVISION USING project-name.
        prog.
-           STRING line-app DELIMITED BY ":"
-           project-name DELIMITED BY SPACES
-           INTO buffer
+           STRING line-tit DELIMITED BY ":"
+             project-name DELIMITED BY SPACES
+             INTO one-line(2)
            END-STRING.
-           DISPLAY buffer END-DISPLAY.
-           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 7
-               MOVE "#" TO buffer
+           STRING line-app DELIMITED BY ":"
+             project-name DELIMITED BY SPACES
+             INTO one-line(6)
+           END-STRING.
+
+           PERFORM VARYING i FROM 1 BY 1 UNTIL i > 13
+      *        MOVE "#" TO buffer
                MOVE one-line(i) to buffer
                CALL "trimright" USING CONTENT buffer
            END-PERFORM.
--- mk-cobol-project/trimright.cbl
+++ mk-cobol-project/trimright.cbl
@@ -5,15 +5,23 @@
        WORKING-STORAGE SECTION.
        01 blen PIC 99999.
        LINKAGE SECTION.
-      *> project name, the first argument on the command line
        01 full-string pic x ANY LENGTH.
 
        PROCEDURE DIVISION USING full-string.
        prog.
            MOVE FUNCTION LENGTH ( FUNCTION TRIM (full-string TRAILING) )
            TO blen.
-           DISPLAY full-string(1:blen)
-           END-DISPLAY
+           DISPLAY blen " -- " END-DISPLAY.
+           if blen > 0 then
+              if blen = 256 then
+                display full-string(1:20) end-display
+              else
+                DISPLAY full-string(1:blen) END-DISPLAY
+              end-if
+           else
+              DISPLAY '##'
+              END-DISPLAY
+           end-if.
            EXIT PROGRAM.
 
            END PROGRAM trimright.
