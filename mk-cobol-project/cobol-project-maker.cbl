83a1ebabc34de58ef0a226d3d8b76043b744c758
Bernard Tatin <bernard.tatin@outlook.fr>
2016-10-21 22:33:04 +0200

    Some white spaces?

--- mk-cobol-project/cobol-project-maker.cbl
+++ mk-cobol-project/cobol-project-maker.cbl
@@ -9,14 +9,14 @@
 
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-      *> project name, the first argument on the command line   
+      *> project name, the first argument on the command line
            01 project-name PIC X(256).
 
       *> program entry point
        PROCEDURE DIVISION.
       *>   Program inirialisation
            perform 001-INIT.
-      *>   if we are her, init is good enough, tell ti to user        
+      *>   if we are her, init is good enough, tell ti to user
            DISPLAY "We will create the project " project-name
            END-DISPLAY.
       *>   create Makefile
@@ -26,24 +26,24 @@
            CALL "create-cobolfile" USING CONTENT project-name
            END-CALL.
            perform 900-TERMINATE.
-           
+
        001-INIT SECTION.
       *>   reading the first argument of the comand line
       *>   put it in project-name.
       *>   Open Cobol specific I think, set -std of cobc at standard.
-      *>   TODO find a way to control string size here 
+      *>   TODO find a way to control string size here
            ACCEPT project-name FROM COMMAND-LINE
            END-ACCEPT.
            IF project-name = SPACES
       *>       if no arguments, show an error
-      *>       under Unix, it's like writing on stderr     
-               DISPLAY "cobol-project-maker needs a project file name" 
+      *>       under Unix, it's like writing on stderr
+               DISPLAY "cobol-project-maker needs a project file name"
                  UPON SYSERR
                END-DISPLAY
       *>       ... and exit
                perform 900-TERMINATE
            END-IF.
-           
+
        900-TERMINATE SECTION.
            STOP RUN.
 
