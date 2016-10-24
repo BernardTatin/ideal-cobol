f5d331fd8a966d59625f26b797d14afdc67accb3
Bernard Tatin <bernard.tatin@outlook.fr>
2016-10-25 00:01:06 +0200

    I can compile and execute zcobol with bash scripts

--- z390/exec.sh
+++ z390/exec.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-script=$(basename $0)
 ici=$(dirname $0)
-
-onerror() {
-    local exit_code=$1
-    local error_msg=$2
-
-    echo "ERROR: $error_msg" 1>&2
-    exit $exit_code
-}
-
-with_echo=0
-case $1 in
-	tron|TRON)
-		with_echo=1
-		shift
-		;;
-	troff|TROFF)
-		with_echo=0
-		shift
-		;;
-esac
+source ${ici}/shlib.sh
 
 # exec execute 390 load module
 rm -fv $1.log $1.err $1.sta $1.tr*
