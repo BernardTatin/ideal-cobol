636cd97bacd54ae1aae2e99b2d23e5e8fbc48977
Bernard (cobol) Tatin <bernard.tatin@outlook.fr>
2019-09-18 11:37:03 +0200

    test-date.sh: more readable output

--- z390-demo/bin/test-date.sh
+++ z390-demo/bin/test-date.sh
@@ -9,12 +9,15 @@ mkdir -p ${tmpdir}
 cd ${tmpdir}
 
 date -R
-echo "Compile and run with RainCode Cobol"
+echo
+echo ">>> Compile and run with RainCode Cobol"
 cobrc :StrictIBM=TRUE ../DATETIME.CBL \
   || { echo "cobrc failed"; exit 1; }
+echo '...'
 rclrun DATETIME.dll
-
-echo "Compile and run with GNU Cobol"
+echo
+echo ">>> Compile and run with GNU Cobol"
 cobc -std=ibm-strict -Wcolumn-overflow -Wall -Wterminator -W -fixed -x -o DATETIME.EXE ../DATETIME.CBL \
   || { echo "cobc failed"; exit 1; }
+echo '...'
 ./DATETIME.EXE
